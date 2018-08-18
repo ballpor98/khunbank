493ce60486c1d0896206314d7825f8e321ea5bc4
Piriyapong Laopongsit <peawgg@Piriyapongs-MacBook-Pro.local>
2018-08-18 10:32:56 +0700

    remove experimental intent getters

--- controller.js
+++ controller.js
@@ -4,11 +4,11 @@ const intentApp = new IntentApp()
 intentApp.intent(/.*/, (req, res, next) => {
     console.log(`got intent ${req.body.intent}`)
     next(res)
-}, (req) => req.body.intent)
+})
 
 intentApp.intent('testIntent', (req, res) => {
     res.send('got testIntent')
-}, (req) => req.body.intent)
+})
 
 module.exports = (req, res) => {
     const body = req.body
